cdaff49c000a6c5369fd358061c42d5ef72e08cf
larrygf02 <raulgf_02@hotmail.com>
2020-04-25 17:11:11 -0500

    Init structures app

--- app/app.py
+++ app/app.py
@@ -1,9 +1,15 @@
-from flask import Flask, render_template
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_cors import CORS, cross_origin
+import os
 app = Flask(__name__)
+CORS(app, resources={r"/*": {"origins": "*"}})
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
-@app.route("/")
-def index():
-    return 'Hola mundo desde aqui'
+DB_URL = os.environ["DB_URL"]
+app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+db = SQLAlchemy(app)
+from routes import *
 
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0', port=4000)
